6ed69afcb29b40e7713c53d14c88e901d8d3c2d8
g0odg4mebrother <kirilcukvlad30@gmail.com>
2024-12-25 17:46:47 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApplication44/ConsoleApplication44.cpp
@@ -0,0 +1,31 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(double x) {
+    return sin(x); 
+}
+double Method_Kurt(double a, double b, int n) {
+    double h = (b - a) / n; 
+    double sum = 0.5 * (f(a) + f(b)); 
+
+    for (int i = 1; i < n; ++i) {
+        double x = a + i * h;
+        sum += f(x);
+    }
+
+    return h * sum;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    double a = 0.0;  
+    double b = M_PI; 
+    int n = 1000;  
+
+    double integral = Method_Kurt(a, b, n);
+    cout << "Приблизительное значение интеграла: " << integral << endl;
+
+    return 0;
+}
